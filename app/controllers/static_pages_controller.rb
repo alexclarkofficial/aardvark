391f15273693b58fca7e9ab046a15fffb0d9a9f7
saltandbeer <raditude28@gmail.com>
2014-11-08 16:21:34 -0500

    setup to get twitter mentions per user handle

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,8 +4,17 @@ class StaticPagesController < ApplicationController
     echowrap
     @search_results = Echowrap.song_search(artist: 'Daft Punk')
 
-    @result = twitter.user_timeline('vincestaples')
+    @result = twitter.user_timeline('vincestaples', { exclude_replies: true, count: 200 })
   end
 
+
+  def user
+    @mentions = []
+    twitter.user_timeline(params[:id], { exclude_replies: true, count: 200 }).each do |tweet|
+      tweet.user_mentions.each do |mention|
+        @mentions << mention.attrs[:screen_name]
+      end
+    end
+  end
 
 end
