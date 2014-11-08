cde3b54b846ec9313c11a3f2e242fb85b3c1d9a9
Mike Calvanese <mfcalvanese@gmail.com>
2014-11-08 12:57:32 -0500

    Added Echowrap

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def echowrap
+    Echowrap.configure do |config|
+      config.api_key =       ENV['ECHONEST_API_KEY']
+      config.consumer_key =  ENV['ECHONEST_CONSUMER_KEY']
+      config.shared_secret = ENV['ECHONEST_SHARED_SECRET']
+    end
+
+  end
+
 end
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,8 @@
 class StaticPagesController < ApplicationController
 
   def home
+    echowrap
+    @search_results = Echowrap.song_search(artist: 'Daft Punk')
   end
+
 end
