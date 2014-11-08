1875b02a4808350cec70e7756b89653dae69e74f
saltandbeer <raditude28@gmail.com>
2014-11-08 09:51:10 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Aardvark::Application.config.secret_key_base = '9a7a2d36b5d2270257a11d215114da13ec9e97287c10f4a26e2b9136c6222a085ccbebc34cc90b85c091ad2bbb0b6093375c4ad532629b520471c59342b13a35'
